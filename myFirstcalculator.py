df1fe11032a0615ed84d248d6699475cc6b81f6f
Tammi <tammicarter220@gmail.com>
2020-03-03 02:03:16 -0500

    Create myFirstcalculator.py

---
+++ myFirstcalculator.py
@@ -0,0 +1,30 @@
+'''
+Alghorithims: "The key to programming!"
+Learning Algorithims. Python3
+````````````````````````````````````
+create a calculator that multiplies
+collect user input and print the output, with a message
+'''
+# check connection
+# print ("Hello World")
+
+# Greet User , collect input
+print("Hello There ")
+name = input("What's your name? ")
+# Style :)
+print("*" * 60)
+# ask the user to input a
+print("ok," + name + ", Lets do some multiplication")
+a = input("Type a Number ")
+# ask user to input b
+b = input("Type another Number ")
+# convert a and b into an interger
+a = int(a)
+b = int(b)
+# multiply a * b
+sum = a * b 
+# provide the oupt "sum" from the input to the user 
+print( "The answer is:" , sum)
+
+
+
